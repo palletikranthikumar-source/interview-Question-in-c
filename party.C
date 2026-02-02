7b7ecf3c6c5e5f2ea29e5069d7610be902d48ab5
palletikranthikumar-source <palletikranthikumar@gmail.com>
2026-02-02 15:59:09 +0530

    Add files via upload

---
+++ party.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main() 
+{ 
+  int tea,candy;
+  printf("Enter the party with tea and candy \n");
+  scanf("%d %d",&tea,&candy);
+  if(tea<5||candy<5)
+  {
+      printf("0");
+  }
+  else if(tea>=2*candy||candy>=2*tea)
+  {
+      printf("2");
+  }
+  else
+  {
+      printf("1");
+  }
+  }
